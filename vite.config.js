bd55e70d1fc6d95c7cd8274678691b351070a4fe
Jose Acevedo <jose.acevedo.r@outlook.com>
2023-03-16 17:35:17 -0300

    Fix config para permitir desplegar en GH Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://joseacevedo.github.io/',
+  base: 'https://joseacevedo.github.io//test-github_actions-react/',
   plugins: [react()],
 })
